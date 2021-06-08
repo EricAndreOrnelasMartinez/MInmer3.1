04c71e0932b45b69661c60a88cb1175dd35cf18b
Your Name <you@example.com>
2021-06-07 20:55:24 -0500

    add

--- PHP/nuevo.php
+++ PHP/nuevo.php
@@ -11,15 +11,15 @@ $DatosC = $_POST['DatosC'];
 $SO = $_POST['SO'];
 $Factura = $_POST['Factura'];
 $NumeroP = $_POST['NumeroP'];
-$NumeroC = $_POST['NumeroC'];
-$NumeroT = $_POST['NumeroT'];
-$TipoT = $_POST['TipoT'];
-$Placas = $_POST['Placas']:
-$Operador = $_POST['Operador'];
-$Maniobrista  = $_POST['Maniobrista'];
-$Custodia = $_POST['Custodia'];
 echo json_encode('Todo bien');
 exit;
+// $NumeroC = $_POST['NumeroC'];
+// $NumeroT = $_POST['NumeroT'];
+// $TipoT = $_POST['TipoT'];
+// $Placas = $_POST['Placas']:
+// $Operador = $_POST['Operador'];
+// $Maniobrista  = $_POST['Maniobrista'];
+// $Custodia = $_POST['Custodia'];
 // $HoraSCC = $_POST['HoraSCC']; 
 // $Observaciones = $_POST['Observaciones']; 
 // $sql = "INSERT INTO $Zona(Zona,FechaC,HoraC,FechaE,HoraE,DireccionE,RazonS,DatosC,SO,Factura,NumeroP,NumeroC,NumeroT,TipoT,Placas,Operador,Maniobrista,Custodia,HoraSCC,Observaciones,Terminado) VALUE('$Zona','$FechaC','$HoraC','$FechaE','$HoraE','$DireccionE','$RazonS','$DatosC','$SO','$Factura','$NumeroP','$NumeroC','$NumeroT','$TipoT','$Placas','$Operador','$Maniobrista','$Custodia','$HoraSCC','$Observaciones',0)";
