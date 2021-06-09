f195561ec027784fcb08672d6aaa34bb316cec3b
Your Name <you@example.com>
2021-06-08 22:24:55 -0500

    add

--- tables/index.php
+++ tables/index.php
@@ -10,7 +10,7 @@ $con = mysqli_connect('localhost','root','Lasric.2018','Minmer2');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../">
+    <link rel="stylesheet" href="../CSS/cssforcitis">
     <title><?php echo $city ?></title>
 </head>
 <body>
