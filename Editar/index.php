575b6ed240ebc8ef1bdc66358692fb9dcf26ce6c
Your Name <you@example.com>
2021-06-28 18:41:19 -0500

    add

--- Editar/index.php
+++ Editar/index.php
@@ -10,7 +10,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/styleNC.css">
     <title>Editar</title>
 </head>
 <body>
--- Nuevo/index.php
+++ Nuevo/index.php
@@ -25,7 +25,7 @@ function hasA($string){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../CSS/style.css">
+    <link rel="stylesheet" href="../CSS/styleNC.css">
     <title>Nuevo</title>
 </head>
 <body>
