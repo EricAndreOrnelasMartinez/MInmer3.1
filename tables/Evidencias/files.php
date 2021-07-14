0b4a5bc67c1cc71a2386c413fd65623eba89a1a0
André Ornelas <ericcot2@gmail.com>
2021-07-14 14:01:51 -0400

    add

---
+++ tables/Evidencias/files.php
@@ -0,0 +1,19 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors','1');
+$city = $_GET['city'];
+if(isset($_FILES['archivo']['tmp_name'])){
+    for($_FILES['archivo']['tmp_name'] as $key => $tmp_name){
+        if($_FILES['archivo']['tmp_name'][$key]){
+            $source = $_FILES['myfile']['tmp_name'];
+            $destination = __DIR__.'/'."$city".'/'.$_FILES['myfile']['name'];
+            if(move_uploaded_file($source, $destination)){
+                echo "Los archivos fueron subidos con exito!";
+            }else{
+                echo "hubo un error al subir los archivos";
+            }
+        }
+    }
+
+}
+  
