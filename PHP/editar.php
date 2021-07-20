784116443003d4a4774dd05864a6f1405ef592b9
André Ornelas <ericcot2@gmail.com>
2021-07-20 15:13:30 -0400

    add

--- PHP/editar.php
+++ PHP/editar.php
@@ -23,7 +23,7 @@ $Maniobrista  = $_POST['Maniobrista'];
 $Custodia = $_POST['Custodia'];
 $HoraSCC = $_POST['HoraSCC']; 
 $Observaciones = $_POST['Observaciones'];
-$sqlUpdate = `UPDATE $Zona SET FechaC='$FechaC',HoraC='$HoraC',FechaE='$FechaE',HoraE='$HoraE',DireccionE='$DireccionE',RazonS='$RazonS',DatosC='$DatosC',SO='$SO',Factura='$Factura',NumeroP='$NumeroP',NumeroC='$NumeroC',NumeroT='$NumeroT',TipoT='$TipoT',Placas='$Placas',Operador='$Operador',Maniobrista='$Maniobrista',Custodia='$Custodia',HoraSCC='$HoraSCC',Observaciones='$Observaciones' WHERE ID_SQL=$id`;
+$sqlUpdate = "UPDATE $Zona SET FechaC='.$FechaC.',HoraC='.$HoraC.',FechaE='.$FechaE.',HoraE='.$HoraE.',DireccionE='.$DireccionE.',RazonS='.$RazonS.',DatosC='.$DatosC.',SO='.$SO.',Factura='.$Factura.',NumeroP='.$NumeroP.',NumeroC='.$NumeroC.',NumeroT='.$NumeroT.',TipoT='.$TipoT.',Placas='.$Placas.',Operador='.$Operador.',Maniobrista='.$Maniobrista.',Custodia='.$Custodia.',HoraSCC='.$HoraSCC.',Observaciones='.$Observaciones.' WHERE ID_SQL=$id";
 echo json_encode($sqlUpdate);
 // $resulupdate = mysqli_query($con,$sqlUpdate);
 // $sqlInsert = "INSERT INTO Modifications(Mail,Hour,Day,City,RowN,Description) VALUE('$mail',current_time(),current_date(),'$city',$id,'Editado');";
