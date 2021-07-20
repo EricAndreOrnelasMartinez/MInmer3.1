18174b1952642c92c09cc3e15103d0c341898b63
André Ornelas <ericcot2@gmail.com>
2021-07-20 14:47:00 -0400

    add

--- PHP/editar.php
+++ PHP/editar.php
@@ -18,18 +18,19 @@ $NumeroC = $_POST['NumeroC'];
 $NumeroT = $_POST['NumeroT'];
 $TipoT = $_POST['TipoT'];
 $Placas = $_POST['Placas'];
-$Operador = $_POST['Operador'];
-$Maniobrista  = $_POST['Maniobrista'];
-$Custodia = $_POST['Custodia'];
-$HoraSCC = $_POST['HoraSCC']; 
-$Observaciones = $_POST['Observaciones'];
-$sqlUpdate = "UPDATE $Zona SET FechaC='$FechaC',HoraC='$HoraC',FechaE='$FechaE',HoraE='$HoraE',DireccionE='$DireccionE',RazonS='$RazonS',DatosC='$DatosC',SO='$SO',Factura='$Factura',NumeroP='$NumeroP',NumeroC='$NumeroC',NumeroT='$NumeroT',TipoT='$TipoT',Placas='$Placas',Operador='$Operador',Maniobrista='$Maniobrista',Custodia='$Custodia',HoraSCC='$HoraSCC',Observaciones='$Observaciones' WHERE ID_SQL=$id";
-$resulupdate = mysqli_query($con,$sqlUpdate);
-$sqlInsert = "INSERT INTO Modifications(Mail,Hour,Day,City,RowN,Description) VALUE('$mail',current_time(),current_date(),'$city',$id,'Editado');";
-//$query = mysqli_query($con,$sqlInsert);
-if($resulupdate){
-    echo json_encode('1');
-}else {
-    echo json_encode(mysqli_error($con));
-}
+echo json_encode('todo bien!');
+// $Operador = $_POST['Operador'];
+// $Maniobrista  = $_POST['Maniobrista'];
+// $Custodia = $_POST['Custodia'];
+// $HoraSCC = $_POST['HoraSCC']; 
+// $Observaciones = $_POST['Observaciones'];
+// $sqlUpdate = "UPDATE $Zona SET FechaC='$FechaC',HoraC='$HoraC',FechaE='$FechaE',HoraE='$HoraE',DireccionE='$DireccionE',RazonS='$RazonS',DatosC='$DatosC',SO='$SO',Factura='$Factura',NumeroP='$NumeroP',NumeroC='$NumeroC',NumeroT='$NumeroT',TipoT='$TipoT',Placas='$Placas',Operador='$Operador',Maniobrista='$Maniobrista',Custodia='$Custodia',HoraSCC='$HoraSCC',Observaciones='$Observaciones' WHERE ID_SQL=$id";
+// $resulupdate = mysqli_query($con,$sqlUpdate);
+// $sqlInsert = "INSERT INTO Modifications(Mail,Hour,Day,City,RowN,Description) VALUE('$mail',current_time(),current_date(),'$city',$id,'Editado');";
+// //$query = mysqli_query($con,$sqlInsert);
+// if($resulupdate){
+//     echo json_encode('1');
+// }else {
+//     echo json_encode(mysqli_error($con));
+// }
 ?>
