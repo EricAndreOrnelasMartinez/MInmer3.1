fc6914477dbb43c5b750bdaf31e5532bb7d8c1ad
André Ornelas <ericcot2@gmail.com>
2021-07-22 17:44:29 -0400

    add

--- tables/Img.php
+++ tables/Img.php
@@ -4,7 +4,7 @@ header("Content-Type: image/png");
 
 $txt = "Holaaaaaa!!!, funcionaaaa!!";
 
-$img = imagecreate(500, 500);
+$img = imagecreate(500, 500) or die("no se puede inicializar la cadena");
 $backgroud = imagecolorallocate($img, 220, 109, 0);
 $textcolor = imagecolorallocate($img, 255,255,255);
 imagestring($img, 10,0,0,$txt, $textcolor);
