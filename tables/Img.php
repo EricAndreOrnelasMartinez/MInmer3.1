c2f344bef36ce4e800a3192af3080c416ca6d3cc
André Ornelas <ericcot2@gmail.com>
2021-07-26 23:02:46 -0400

    add

--- tables/Img.php
+++ tables/Img.php
@@ -9,7 +9,7 @@ $res = mysqli_query($con, $sql);
 if($res){
     $he = 5;
     while($show = mysqli_fetch_array($res)){
-        $txt = $txt + "Zona: ".$show['Zona']."  Fecha de carga: ".$show['FechaC']."  Hora de carga: ".$show['HoraC']."  Fecha de entrega: ".$show['FechaE']."  Hora de entrega: ".$show['HoraE']."  Dirección de entrega: ".$show['DireccionE']."  Razón social: ".$show['RazonS']."  Datos del contacto: ".$show['DatosC']."  SO: ".$show['SO']."  Factura: ".$show['Factura']."  Número de piezas: ".$show['NumeroP']."  Número de cajas: ".$show['NumeroC']."  Número de tarimas: ".$show['NumeroT']."  Tipo de unidad: ".$show['TipoT']."  Placas: ".$show['Placas']."  Operador: ".$show['Operador']."  Maniobrista: ".$show['Maniobrista']."  Custodia: ".$show['Custodia']."  Hora de salida con custodia: ".$show['HoraSCC']."  Observaciones: ".$show['Observaciones'];    
+        $txt = "Zona: ".$show['Zona']."  Fecha de carga: ".$show['FechaC']."  Hora de carga: ".$show['HoraC']."  Fecha de entrega: ".$show['FechaE']."  Hora de entrega: ".$show['HoraE']."  Dirección de entrega: ".$show['DireccionE']."  Razón social: ".$show['RazonS']."  Datos del contacto: ".$show['DatosC']."  SO: ".$show['SO']."  Factura: ".$show['Factura']."  Número de piezas: ".$show['NumeroP']."  Número de cajas: ".$show['NumeroC']."  Número de tarimas: ".$show['NumeroT']."  Tipo de unidad: ".$show['TipoT']."  Placas: ".$show['Placas']."  Operador: ".$show['Operador']."  Maniobrista: ".$show['Maniobrista']."  Custodia: ".$show['Custodia']."  Hora de salida con custodia: ".$show['HoraSCC']."  Observaciones: ".$show['Observaciones'];    
         $he = $he + 50;
         $im = @imagecreate(1300, 40);
         $color_fondo = imagecolorallocate($im, 0, 0, 0);
