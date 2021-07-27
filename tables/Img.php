72b190de5d51282f1adac1b65ead1df845dcbd82
André Ornelas <ericcot2@gmail.com>
2021-07-26 20:15:04 -0400

    add

--- tables/Img.php
+++ tables/Img.php
@@ -3,7 +3,7 @@ header("Content-Type: image/png");
 echo "this works";
 $im = @imagecreate(110, 20);
 $color_fondo = imagecolorallocate($im, 0, 0, 0);
-// $color_texto = imagecolorallocate($im, 233, 14, 91);
+$color_texto = imagecolorallocate($im, 233, 14, 91);
 // imagestring($im, 1, 5, 5,  "A Simple Text String", $color_texto);
 // imagepng($im);
 // imagedestroy($im);
