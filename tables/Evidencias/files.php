6ae774f6cac14d21799ed5ac160794e132b82b0a
André Ornelas <ericcot2@gmail.com>
2021-07-26 23:41:57 -0400

    add

--- tables/Evidencias/files.php
+++ tables/Evidencias/files.php
@@ -3,8 +3,8 @@ echo "todo bien";
 $city = $_GET['city'];
     foreach($_FILES['archivo']['tmp_name'] as $key => $tmp_name){
         if($_FILES['archivo']['tmp_name'][$key]){
-            $source = $_FILES['myfile']['tmp_name'][$key];
-            $destination = __DIR__.'/'.$city.'/'.$_FILES['myfile']['name'][$key];
+            $source = $_FILES['archivo']['tmp_name'][$key];
+            $destination = __DIR__.'/'.$city.'/'.$_FILES['archivo']['name'][$key];
             if(move_uploaded_file($source, $destination)){
                 echo "Los archivos fueron subidos con exito!";
             }else{
