f3991e6b7ea3afd608b76700133c582123f68964
André Ornelas <ericcot2@gmail.com>
2021-07-27 09:50:32 -0400

    add

--- tables/Img.php
+++ tables/Img.php
@@ -14,8 +14,8 @@ if($res){
         $im = @imagecreate(400, 400);
         $color_fondo = imagecolorallocate($im, 0, 0, 0);
         $color_texto = imagecolorallocate($im, 233, 14, 91);
-        imagettftext($im, 25, 5, 5, "Zona: ".$show['Zona'] , $color_texto);
-        imagettftext($im, 25, 5, 450, "Fecha de carga: ".$show['FechaC'] , $color_texto);
+        imagestring($im, 25, 5, 5, "Zona: ".$show['Zona'] , $color_texto);
+        imagestring($im, 25, 5, 450, "Fecha de carga: ".$show['FechaC'] , $color_texto);
         imagepng($im);
         imagedestroy($im);
     }
