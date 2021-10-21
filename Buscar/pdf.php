0af156e7e9224e77fafb3d2069d9f7aecb2c52a4
André Ornelas <ericcot2@gmail.com>
2021-10-21 11:53:22 -0500

    add

--- Buscar/pdf.php
+++ Buscar/pdf.php
@@ -8,6 +8,7 @@ $con = mysqli_connect('localhost','root','Lasric.2018','Minmer2');
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',12);
+$pdf->setXY(6,130);
 if($artibute === 'FechaC' || $artibute === 'FechaE'){
     $sql = "SELECT * FROM  $city WHERE $artibute BETWEEN '$query' AND '$query2'";
     $resSQL = mysqli_query($con, $sql);
@@ -15,10 +16,11 @@ if($artibute === 'FechaC' || $artibute === 'FechaE'){
         $pdf->Cell(90,10,utf8_decode($show['Zona']), 0, 1, 'R', 0);
         $pdf->Cell(90,10,'Fecha de entrega: '.utf8_decode($show['FechaE']), 0, 1, 'C', 0);
         $pdf->Cell(90,10,'Hora de entrega: '.utf8_decode($show['HoraE']), 0, 1, 'C', 0);
-        $pdf->Cell(90,10, utf8_decode('Dirección de entrega: ').utf8_decode($show['DireccionE']), 0, 1, 'C', 0);
-        $pdf->Cell(90,10, utf8_decode('Datos de contacto: ').utf8_decode($show['DatosC']), 0, 1, 'C', 0);
+        $pdf->MultiCell(90,10, utf8_decode('Dirección de entrega: ').utf8_decode($show['DireccionE']), 0, 1, 'C', 0);
+        $pdf->MultiCell(90,10, utf8_decode('Datos de contacto: ').utf8_decode($show['DatosC']), 0, 1, 'C', 0);
         $pdf->Cell(90,10, utf8_decode('Razón social: ').utf8_decode($show['RazonS']), 0, 1, 'C', 0);
         $pdf->Cell(90,10, utf8_decode('Factura: ').utf8_decode($show['Factura']), 0, 1, 'C', 0);
+        $pdf->MultiCell(90,10, utf8_decode('Observaciones: ').utf8_decode($show['Observaciones']), 0, 1, 'C', 0);
     }
 }else{
     $sql = "SELECT * FROM $city WHERE $artibute='$query'";
@@ -27,10 +29,11 @@ if($artibute === 'FechaC' || $artibute === 'FechaE'){
         $pdf->Cell(90,10,utf8_decode($show['Zona']), 0, 1, 'C', 0);
         $pdf->Cell(90,10,'Fecha de entrega: '.utf8_decode($show['FechaE']), 0, 1, 'C', 0);
         $pdf->Cell(90,10,'Hora de entrega: '.utf8_decode($show['HoraE']), 0, 1, 'C', 0);
-        $pdf->Cell(90,10, utf8_decode('Dirección de entrega: ').utf8_decode($show['DireccionE']), 0, 1, 'C', 0);
-        $pdf->Cell(90,10, utf8_decode('Datos de contacto: ').utf8_decode($show['DatosC']), 0, 1, 'C', 0);
+        $pdf->MultiCell(90,10, utf8_decode('Dirección de entrega: ').utf8_decode($show['DireccionE']), 0, 1, 'C', 0);
+        $pdf->MultiCell(90,10, utf8_decode('Datos de contacto: ').utf8_decode($show['DatosC']), 0, 1, 'C', 0);
         $pdf->Cell(90,10, utf8_decode('Razón social: ').utf8_decode($show['RazonS']), 0, 1, 'C', 0);
         $pdf->Cell(90,10, utf8_decode('Factura: ').utf8_decode($show['Factura']), 0, 1, 'C', 0);
+        $pdf->MultiCell(90,10, utf8_decode('Observaciones: ').utf8_decode($show['Observaciones']), 0, 1, 'C', 0);
     }
 }
 $pdf->Output();
